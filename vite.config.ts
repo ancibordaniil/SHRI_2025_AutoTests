7d4b3db2f61d9a265809f769fb553370473d8393
Antsibor Daniil <s1lence0073777@gmail.com>
2025-06-27 14:33:25 +0300

    [update]: init back + front

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
@@ -29,5 +27,11 @@ export default defineConfig(({ mode }) => {
             },
         },
         plugins: [react()],
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: ['./src/setupTests.ts'], 
+            include: ['src/**/*.test.{ts,tsx}'],
+        },
     };
 });
